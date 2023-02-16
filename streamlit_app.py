880dcb0721925fdd50649303a05b29259b4e08e9
danielsand1984 <100784830+danielsand1984@users.noreply.github.com>
2023-02-16 14:59:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit
+import pandas
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+//Web page 
 streamlit.title('My parents now have a healthy diner')
 
 
